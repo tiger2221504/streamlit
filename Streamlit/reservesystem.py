fc6c79d51eb287ef6a1cdc926980b6a44c70fc02
tiger2221504 <96847677+tiger2221504@users.noreply.github.com>
2023-11-16 23:08:17 +0900

    Update reservesystem.py

--- Streamlit/reservesystem.py
+++ Streamlit/reservesystem.py
@@ -54,9 +54,9 @@ def MakeDf(worksheet):
 
 #ページコンフィグ
 st.set_page_config(
-     page_title="yoyaku",
+     page_title="Yoyaku",
      menu_items={
-         'Google spreadsheet': "https://docs.google.com/spreadsheets/d/1LkW6x8rBrNog_ynW7Dvs4rEGmdPGbPm9AYa7VHMG650/edit?usp=sharing",
+         'Get help': "https://docs.google.com/spreadsheets/d/1LkW6x8rBrNog_ynW7Dvs4rEGmdPGbPm9AYa7VHMG650/edit?usp=sharing",
          'About': "# GHK"
      }
  )
